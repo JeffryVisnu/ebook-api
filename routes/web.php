5ba881f741ae008baf392708df82f392596cfd5b
JeffryVisnu <91933021+JeffryVisnu@users.noreply.github.com>
2022-10-17 14:13:26 +0700

    endpoint

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,32 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+//string
 Route::get('coba',function() {
     return "Haloo ^^";
+});
+
+//array
+Route::get('coba1',function(){
+    return ['jepri','yp','tanjung','jj'];
+});
+
+//json objek
+Route::get('coba2',function(){
+    return [
+        "Nama" => "Jeffry Kurniawan Visnu",
+        "Kelas" => "XII RPL 4",
+        "NIS" => 3103120116
+    ];
+});
+
+//objek json ubah status code
+Route::get("coba3",function(){
+    return response()->json(
+        [
+            "Nama" => "Jeffry Kurniawan Visnu",
+            "Kelas" => "XII RPL 4",
+            "NIS" => 3103120116
+        ], 201
+    );
 });
